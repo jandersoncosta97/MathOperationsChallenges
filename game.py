08238cbc6f8b088d086d499de213f09226959044
Janderson Lima Costa <janderson.costa@ufpe.br>
2025-03-09 16:00:33 -0300

    DOCS: Docstring

--- game.py
+++ game.py
@@ -1,10 +1,17 @@
 from models.calcular import Calcular
 
+
 def main() -> None:
+    """Responsável por executar jogar(), e inicializar os pontos como nulo.
+    """
     pontos: int = 0
     jogar(pontos)
 
+
 def jogar(pontos: int) -> None:
+    """Responsável pela interação do usuário com a interface do jogo, incluindo gerenciamento de dificuldade,
+    utilização dos métodos de Calcular. Finalmente, é responsável pelas impressão de resultados.
+    """
     dificuldade: int = int(input('Informe o nível de dificuldade desejado [1, 2, 3 ou 4]: '))
 
     calc: Calcular = Calcular(dificuldade)
@@ -13,7 +20,7 @@ def jogar(pontos: int) -> None:
     calc.mostrar_operacao()
 
     resultado: int = int(input())
-    
+
     if calc.checar_resultado(resultado):
         pontos += 1
     print(f'Você possui {pontos} ponto' if pontos <= 1 else f'Você possui {pontos} pontos')
@@ -27,8 +34,5 @@ def jogar(pontos: int) -> None:
         print(f'Você finalizou com {pontos} ponto' if pontos <= 1 else f'Você finalizou com {pontos} pontos')
         print('Até a próxima!')
 
-
-    
 if __name__ == '__main__':
     main()
-    
--- models/calcular.py
+++ models/calcular.py
@@ -1,34 +1,36 @@
 from random import randint
 
+
 class Calcular:
-    
+    """Responsável pelas geração das operações matemáticas
+    """
     def __init__(self: object, dificuldade: int, /) -> None:
         self.__dificuldade : int = dificuldade
         self.__valor1: int = self._gerar_valor
         self.__valor2: int = self._gerar_valor
         self.__operacao: int = randint(1, 3) # 1 = somar, 2 = diminuir, 3 = multiplicar
         self.__resultado: int = self._gerar_resultado
-        
+
     @property
     def dificuldade(self: object) -> int:
         return self.__dificuldade
-    
+
     @property
     def valor1(self: object) -> int:
         return self.__valor1
-    
+
     @property
     def valor2(self: object) -> int:
         return self.__valor2
-    
+
     @property
     def operacao(self: object) -> int:
         return self.__operacao
-    
+
     @property
     def resultado(self: object) -> int:
         return self.__resultado
-    
+
     def __str__(self: object) -> str:
         op: str = ''
         if self.operacao == 1:
@@ -40,7 +42,7 @@ class Calcular:
         else:
             op = 'Operacao desconhecida'
         return f"Valor1: {self.valor1}\nValor2: {self.valor2}\nDificuldade: {self.dificuldade}\nOperação: {op}"
-    
+
     @property
     def _gerar_valor(self: object) -> int:
         if self.dificuldade == 1:
@@ -53,7 +55,7 @@ class Calcular:
             return randint(0, 10000)
         else:
             return randint(0, 100000)
-    
+
     @property
     def _gerar_resultado(self: object) -> int:
         if self.operacao == 1: # somar
@@ -62,7 +64,7 @@ class Calcular:
             return self.valor1 - self.valor2
         else:
             return self.valor1 * self.valor2
-    
+
     @property
     def _op_simbolo(self: object) -> str:
         if self.operacao == 1:
